ad18cc9d946b2f96ddeb38774dc505e6a913eb0d
Vincent Bitter <vincent@vincentbitter.nl>
2020-12-31 15:45:45 +0100

    Fix test

--- src/test/pages/index.test.tsx
+++ src/test/pages/index.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import client, { Session } from 'next-auth/client'
 import { render } from '../testUtils'
 import { Home } from '../../pages/index'
 
+jest.mock('next-auth/client')
+
 describe('Home page', () => {
   it('matches snapshot', () => {
+    const mockSession: Session = {
+      expires: '1',
+      user: { email: 'a', name: 'Delta', image: 'c' },
+    }
+
+    ;(client.useSession as jest.Mock).mockReturnValueOnce([mockSession, false])
+
     const { asFragment } = render(<Home />, {})
     expect(asFragment()).toMatchSnapshot()
   })
